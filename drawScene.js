630c7f6e053c77f2684c02e3a5b39757969aefc7
Unknown <jnfitzpa@iupui.edu>
2019-08-03 13:42:45 -0400

    Transition between all scenes

--- drawScene.js
+++ drawScene.js
@@ -1,6 +1,10 @@
+//Set the text for the messages
 var messages_text =  ["I'm down for this first chart"
 ,"Bro this second chart is sick"
 ,"Dude! This third chart is far out!"];
+
+
+
 
 function init() {
   draw_scene(1);
@@ -15,6 +19,9 @@ async function draw_scene(scene_num){
     prevEl.style.backgroundColor = 'silver';
     prevEl.style.opacity = '.3';
     document.getElementById('next').removeAttribute("style");
+    //var parent = document.getElementById("body");
+    //var child = document.getElementsByClassName("tooltip");
+    //parent.removeChild(child);
 
     const data = await d3.csv('https://jnfitzpa.github.io/AvgRateByAge.csv');
     console.log(data);
@@ -36,7 +43,7 @@ async function draw_scene(scene_num){
     var yScale = d3.scaleBand().domain(ages).range([0,height]);
 
     // Define the div for the tooltip
-    var div = d3.select("body").append("div")
+    var tt = d3.select("#tooltipdiv")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
@@ -54,15 +61,15 @@ async function draw_scene(scene_num){
     .attr("y",function(d,i) { return (barThickness+1.0)*i; })
     .on("mouseover", function(d) {
       var prem = parseFloat(d.avg);
-      div.transition()
+      tt.transition()
           .duration(200)
           .style("opacity", .9);
-      div	.html("Age: " + d.age + "<br/>"  + "Premium: $" + prem.toFixed(2))
+      tt	.html("Age: " + d.age + "<br/>"  + "Premium: $" + prem.toFixed(2))
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY - 28) + "px");
       })
     .on("mouseout", function(d) {
-      div.transition()
+      tt.transition()
           .duration(500)
           .style("opacity", 0)
       });
@@ -111,6 +118,102 @@ async function draw_scene(scene_num){
     document.getElementById('prev').removeAttribute("style");
     document.getElementById('next').removeAttribute("style");
 
+    const data = await d3.csv('https://jnfitzpa.github.io/AvgRateByAge.csv');
+    console.log(data);
+    var ages = [];
+    data.forEach(function(value) {
+      ages.push(value.age);
+    });
+    console.log(ages);
+    var lmargin = 80;
+    var rmargin = 10;
+    var tmargin = 10;
+    var bmargin = 50;
+    var frameWidth = 800;
+    var frameHeight = 500;
+    var width = frameWidth-lmargin-rmargin;
+    var height = frameHeight-tmargin-bmargin;
+    var barThickness = height/(data.length) - 1.0;
+    var xScale = d3.scaleLinear().domain([0,600]).range([0,width]);
+    var yScale = d3.scaleBand().domain(ages).range([0,height]);
+
+    // Define the div for the tooltip
+    var tt = d3.select("#tooltipdiv")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+
+    //document.getElementById("data").innerHTML = data.avg;
+    d3.select(".chart")
+    .attr("width",frameWidth)
+    .attr("height",frameHeight)
+    .append("g").attr("transform","translate("+lmargin+","+tmargin+")")
+    .selectAll("rect").data(data)
+    .enter().append("rect")
+    .attr("width",function(d) { return xScale(d.avg);})
+    .attr("height", barThickness)
+    .attr("x",function(d,i) { lmargin; })
+    .attr("y",function(d,i) { return (barThickness+1.0)*i; })
+    .on("mouseover", function(d) {
+      var prem = parseFloat(d.avg);
+      tt.transition()
+          .duration(200)
+          .style("opacity", .9);
+      tt	.html("Age: " + d.age + "<br/>"  + "Premium: $" + prem.toFixed(2))
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      })
+    .on("mouseout", function(d) {
+      tt.transition()
+          .duration(500)
+          .style("opacity", 0)
+      });
+
+
+    d3.select(".chart")
+    .append("g")
+    .attr("transform","translate("+lmargin+","+tmargin+")")
+    .call(
+      d3.axisLeft(yScale)
+      .tickValues(ages)
+    );
+    d3.select(".chart")
+    .append("g")
+    .attr("transform","translate("+lmargin+","+(height+tmargin)+")")
+    .call(
+      d3.axisBottom(xScale)
+      .tickValues([100, 200, 300, 400, 500, 600])
+      .tickFormat(d3.format("~s"))
+    );
+
+    // text label for the x axis
+    d3.select(".chart")
+    .append("text")
+    .attr("transform","translate(" + (frameWidth/2) + " ," + (frameHeight-.1*bmargin) + ")")
+    .style("text-anchor", "middle")
+    .text("Average Premium Cost ($)");
+
+    // text label for the y axis
+    d3.select(".chart")
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Subscriber's Age");
+
+    d3.select("#message > p").text(messages_text[scene_num-1]);
+
+  } else {
+    document.getElementById("magic").innerHTML = "";
+    d3.select("#prev").attr("onclick","draw_scene(2)");
+    d3.select("#next").attr("onclick","");
+    document.getElementById('prev').removeAttribute("style");
+    var nextEl = document.getElementById('next');
+    nextEl.style.backgroundColor = 'silver';
+    nextEl.style.opacity = '.3';
+
 
 
     const data = await d3.csv('https://jnfitzpa.github.io/AvgRateByFamilyType.csv');
@@ -132,8 +235,8 @@ async function draw_scene(scene_num){
   	var xScale = d3.scaleLinear().domain([0,110]).range([0,width]);
   	var yScale = d3.scaleBand().domain(family_types).range([0,height]);
 
-  	// Define the div for the tooltip
-  	var div = d3.select("body").append("div")
+    // Define the div for the tooltip
+    var tt = d3.select("#tooltipdiv")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
@@ -151,15 +254,15 @@ async function draw_scene(scene_num){
   	.attr("y",function(d,i) { return (barThickness+1.0)*i; })
   	.on("mouseover", function(d) {
   		var prem = parseFloat(d.rate);
-  		div.transition()
+  		tt.transition()
   				.duration(200)
   				.style("opacity", .9);
-  		div	.html("Family Type: " + d.family_type + "<br/>"  + "Premium: $" + prem.toFixed(2))
+  		tt	.html("Family Type: " + d.family_type + "<br/>"  + "Premium: $" + prem.toFixed(2))
   				.style("left", (d3.event.pageX) + "px")
   				.style("top", (d3.event.pageY - 28) + "px");
   		})
   	.on("mouseout", function(d) {
-  		div.transition()
+  		tt.transition()
   				.duration(500)
   				.style("opacity", 0)
   		});
@@ -199,6 +302,8 @@ async function draw_scene(scene_num){
     .text("Family Type");
 
     d3.select("#message > p").text(messages_text[scene_num-1]);
+
   }
+
 
 }
